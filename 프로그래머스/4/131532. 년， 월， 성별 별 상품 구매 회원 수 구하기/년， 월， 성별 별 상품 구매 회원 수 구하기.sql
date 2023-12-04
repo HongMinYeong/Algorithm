3e1f3cc4af2fb7c32adcb7af5a1367f6e6f3c5d5
BOSS <65701100+HongMinYeong@users.noreply.github.com>
2023-12-04 21:54:38 +0900

    [level 4] Title: 년, 월, 성별 별 상품 구매 회원 수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/년， 월， 성별 별 상품 구매 회원 수 구하기.sql
+++ 프로그래머스/4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/년， 월， 성별 별 상품 구매 회원 수 구하기.sql
@@ -8,3 +8,24 @@ JOIN USER_INFO B ON A.USER_ID = B.USER_ID
 WHERE GENDER IS NOT NULL
 GROUP BY YEAR(SALES_DATE),MONTH(SALES_DATE),GENDER
 ORDER BY YEAR(SALES_DATE),MONTH(SALES_DATE),GENDER;
+
+# SELECT 절:
+
+# YEAR(SALES_DATE) AS YEAR: SALES_DATE 열에서 연도를 추출하고 "YEAR"라는 별칭을 부여합니다.
+# MONTH(SALES_DATE) AS MONTH: SALES_DATE 열에서 월을 추출하고 "MONTH"라는 별칭을 부여합니다.
+# GENDER: 사용자의 성별 정보를 나타냅니다.
+# COUNT(DISTINCT A.USER_ID) AS USERS: 고유 사용자 수를 계산하여 "USERS"라는 별칭을 부여합니다.
+# FROM 절:
+
+# ONLINE_SALE A: "ONLINE_SALE" 테이블을 "A"라는 별칭으로 지정합니다.
+# USER_INFO B: "USER_INFO" 테이블을 "B"라는 별칭으로 지정합니다.
+# JOIN USER_ID = B.USER_ID: "A" 테이블의 USER_ID와 "B" 테이블의 USER_ID를 기준으로 두 테이블을 결합합니다.
+# WHERE 절:
+
+# GENDER IS NOT NULL: 성별 정보가 있는 경우에 해당하는 행만 선택합니다.
+# GROUP BY 절:
+
+# GROUP BY YEAR(SALES_DATE), MONTH(SALES_DATE), GENDER: 연도, 월 및 성별에 따라 데이터를 그룹화합니다.
+# ORDER BY 절:
+
+# ORDER BY YEAR(SALES_DATE), MONTH(SALES_DATE), GENDER: 연도, 월, 성별 순서로 결과를 정렬합니다.
