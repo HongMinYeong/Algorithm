f528f235d3348342f07ead1fb75ba3b2d8efed8f
BOSS <65701100+HongMinYeong@users.noreply.github.com>
2024-05-01 22:38:49 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = new int[num2-num1+1];
+        int pos = 0;
+        for(int i=0;i<numbers.length;i++){
+            if(i==num1){
+                for(int j=num1;j<=num2;j++){
+                    answer[pos]=numbers[j];
+                    pos++;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
