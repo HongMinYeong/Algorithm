8531cc294f1d670aca8e3a1b9c0fe934a92a6832
BOSS <65701100+HongMinYeong@users.noreply.github.com>
2024-05-01 23:20:03 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.34 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -1,16 +1,26 @@
+// import java.util.*;
+
+// class Solution {
+//     public int solution(int[] numbers) {
+//         int answer = 0;
+//         int temp=0;
+//         Arrays.sort(numbers);
+//         answer=numbers[numbers.length-1]*numbers[numbers.length-2];
+//         if(numbers[0]<0){
+//             temp = numbers[0]*numbers[1];
+//             answer=temp>answer?temp:answer;
+//         }
+      
+//         return answer;
+//     }
+// }
 import java.util.*;
 
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
-        int temp=0;
+        int len = numbers.length;
         Arrays.sort(numbers);
-        answer=numbers[numbers.length-1]*numbers[numbers.length-2];
-        if(numbers[0]<0){
-            temp = numbers[0]*numbers[1];
-            answer=temp>answer?temp:answer;
-        }
-      
-        return answer;
+        return Math.max(numbers[0] * numbers[1], numbers[len - 2] * numbers[len - 1]);
     }
 }
+
