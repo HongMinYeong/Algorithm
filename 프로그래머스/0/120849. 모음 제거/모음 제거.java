4d98b2dfc2e2b2ce87a4668fe47bd53f46805d77
BOSS <65701100+HongMinYeong@users.noreply.github.com>
2024-05-01 23:12:22 +0900

    [level 0] Title: 모음 제거, Time: 0.04 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,13 +1,25 @@
+// class Solution {
+//     public String solution(String my_string) {
+//          StringBuilder answer = new StringBuilder();
+
+//         for (char c : my_string.toCharArray()) {
+//             if (c != 'a'&& c!='e'&&c!='i'&&c!='o'&&c!='u') {
+//                 answer.append(c);
+//                 // c != 'a'|| c!='e'||c!='i'||c!='o'||c!='u'
+//             }
+//         }
+//         return answer.toString();
+//     }
+// }
 class Solution {
     public String solution(String my_string) {
-         StringBuilder answer = new StringBuilder();
-
-        for (char c : my_string.toCharArray()) {
-            if (c != 'a'&& c!='e'&&c!='i'&&c!='o'&&c!='u') {
-                answer.append(c);
-                // c != 'a'|| c!='e'||c!='i'||c!='o'||c!='u'
+        String[] vowels = new String[]{"a", "e", "i", "o", "u"};
+        for(String vowel : vowels){
+            if(my_string.contains(vowel)){
+                my_string = my_string.replace(vowel, "");
             }
         }
-        return answer.toString();
+        return my_string;
     }
 }
+
