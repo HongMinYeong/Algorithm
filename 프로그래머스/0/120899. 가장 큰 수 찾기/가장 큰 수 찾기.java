f19f03c6b899546fb24f78cf9da895cc4ad9a9d8
BOSS <65701100+HongMinYeong@users.noreply.github.com>
2024-05-01 20:02:33 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -1,20 +1,34 @@
-import java.util.*;
+// import java.util.*;
 
+// class Solution {
+//     public int[] solution(int[] array) {
+//         ArrayList<Integer> answer = new ArrayList<>();
+//         int[] sortedArray = array.clone(); 
+//         Arrays.sort(sortedArray); 
+
+//         int maxValue = sortedArray[sortedArray.length - 1]; 
+//         answer.add(maxValue); 
+
+//         for (int i = 0; i < array.length; i++) {
+//             if (array[i] == maxValue) { 
+//                 answer.add(i); 
+//             }
+//         }
+
+//         return answer.stream().mapToInt(Integer::intValue).toArray(); 
+//     }
+// }
 class Solution {
     public int[] solution(int[] array) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        int[] sortedArray = array.clone(); 
-        Arrays.sort(sortedArray); 
+        int[] answer = new int[2];
 
-        int maxValue = sortedArray[sortedArray.length - 1]; 
-        answer.add(maxValue); 
-
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == maxValue) { 
-                answer.add(i); 
+        for(int i=0;i<array.length;i++) {
+            if(array[i] > answer[0]) {
+                answer[0] = array[i];
+                answer[1] = i;
             }
         }
 
-        return answer.stream().mapToInt(Integer::intValue).toArray(); 
+        return answer;
     }
 }
