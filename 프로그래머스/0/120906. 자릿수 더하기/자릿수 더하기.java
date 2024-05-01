c16595ddd4f7d87bc4dc4acc3d939a0faf1055e5
BOSS <65701100+HongMinYeong@users.noreply.github.com>
2024-05-01 22:12:45 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,12 +1,25 @@
+// class Solution {
+//     public int solution(int n) {
+//         int answer = 0;
+//         String str = Integer.toString(n);
+
+//         for (char c : str.toCharArray()) {
+//             answer += Character.getNumericValue(c);
+//         }
+
+//         return answer;
+//     }
+// }
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        String str = Integer.toString(n);
 
-        for (char c : str.toCharArray()) {
-            answer += Character.getNumericValue(c);
+        while(n>0){
+            answer+=n%10;
+            n/=10;
         }
 
         return answer;
     }
 }
+
