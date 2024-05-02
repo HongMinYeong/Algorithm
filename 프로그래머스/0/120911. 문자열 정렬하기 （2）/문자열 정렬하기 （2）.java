364b29507aa0c9b9a5aed1055da3d1c0df021d2c
BOSS <65701100+HongMinYeong@users.noreply.github.com>
2024-05-02 22:37:38 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.49 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -1,20 +1,29 @@
-import java.util.*; 
+// import java.util.*; 
 
+// class Solution {
+//     public String solution(String my_string) {
+//         String answer = "";
+//         int[] list = new int[my_string.length()];
+//         for(int i = 0;i<my_string.length();i++){
+//          if (my_string.charAt(i)<97){
+//              list[i]=my_string.charAt(i)+32;
+//          }else{
+//              list[i]=my_string.charAt(i);
+//          }
+//         }
+//         Arrays.sort(list);
+//         for(int i=0;i<list.length;i++){
+//             answer+=Character.toString(list[i]);
+//         }
+//         return answer;
+//     }
+// }
+import java.util.*;
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-        int[] list = new int[my_string.length()];
-        for(int i = 0;i<my_string.length();i++){
-         if (my_string.charAt(i)<97){
-             list[i]=my_string.charAt(i)+32;
-         }else{
-             list[i]=my_string.charAt(i);
-         }
-        }
-        Arrays.sort(list);
-        for(int i=0;i<list.length;i++){
-            answer+=Character.toString(list[i]);
-        }
-        return answer;
+        char[] c = my_string.toLowerCase().toCharArray();
+        Arrays.sort(c);
+        return new String(c);
     }
 }
+
