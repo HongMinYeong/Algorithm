c6d94a9a25acadeea7837612333f757a3fc8d8d4
BOSS <65701100+HongMinYeong@users.noreply.github.com>
2024-05-02 22:36:34 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 1.99 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,20 @@
+import java.util.*; 
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        int[] list = new int[my_string.length()];
+        for(int i = 0;i<my_string.length();i++){
+         if (my_string.charAt(i)<97){
+             list[i]=my_string.charAt(i)+32;
+         }else{
+             list[i]=my_string.charAt(i);
+         }
+        }
+        Arrays.sort(list);
+        for(int i=0;i<list.length;i++){
+            answer+=Character.toString(list[i]);
+        }
+        return answer;
+    }
+}
