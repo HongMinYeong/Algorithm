b9f0cf895a64705e310ef7d15834c14f80f81155
BOSS <65701100+HongMinYeong@users.noreply.github.com>
2024-05-02 17:52:11 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,11 +1,16 @@
-class Solution {
-    public String solution(String my_string) {
-        StringBuilder answer = new StringBuilder();
-        char[] data = my_string.toCharArray();
-        for(int i=0;i<data.length;i++){
-            answer.append(data[data.length-1-i]);
-        }
+// class Solution {
+//     public String solution(String my_string) {
+//         StringBuilder answer = new StringBuilder();
+//         char[] data = my_string.toCharArray();
+//         for(int i=0;i<data.length;i++){
+//             answer.append(data[data.length-1-i]);
+//         }
         
-        return answer.toString();
+//         return answer.toString();
+//     }
+// }
+class Solution {
+    public String solution(String myString) {
+        return new StringBuilder(myString).reverse().toString();
     }
 }
