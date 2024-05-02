b9aef43d1c66fa3117760a276168b6c9ae1a5ae2
BOSS <65701100+HongMinYeong@users.noreply.github.com>
2024-05-02 22:11:22 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,19 +1,36 @@
+// class Solution {
+//     public int solution(int num, int k) {
+//         int answer = 0;
+//         //num을 char 배열로 
+//         String str = Integer.toString(num);
+//         int pos = 0;
+//         for(char c:str.toCharArray()){
+//             if (c-'0'==k){
+//                 answer=pos+1;
+//                 break;
+//             }
+//             pos++;
+//         }
+//         if (answer==0){
+//             answer=-1;
+//         }
+//         return answer;
+//     }
+// }
 class Solution {
     public int solution(int num, int k) {
-        int answer = 0;
-        //num을 char 배열로 
-        String str = Integer.toString(num);
-        int pos = 0;
-        for(char c:str.toCharArray()){
-            if (c-'0'==k){
-                answer=pos+1;
+        int answer = -1;
+
+        String str = String.valueOf(num);
+
+        for(int i =0; i<str.length(); i++){
+            if(str.charAt(i) == k + '0'){
+                answer = i+1;
                 break;
             }
-            pos++;
         }
-        if (answer==0){
-            answer=-1;
-        }
+
         return answer;
     }
 }
+
