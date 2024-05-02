669b3bbb57dae88a7f5c57e9a41621e72170270e
BOSS <65701100+HongMinYeong@users.noreply.github.com>
2024-05-02 22:24:51 +0900

    [level 0] Title: 369게임, Time: 7.20 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.java
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -1,12 +1,27 @@
+// class Solution {
+//     public int solution(int order) {
+//         int answer = 0;
+//         String str = Integer.toString(order);
+//         for(int i=0;i<str.length();i++){
+//          if ((str.charAt(i)-'0')==3 || (str.charAt(i)-'0')==6 || (str.charAt(i)-'0')==9){
+//              answer++;
+//          }   
+//         }
+//         return answer;
+//     }
+// }
 class Solution {
     public int solution(int order) {
         int answer = 0;
-        String str = Integer.toString(order);
-        for(int i=0;i<str.length();i++){
-         if ((str.charAt(i)-'0')==3 || (str.charAt(i)-'0')==6 || (str.charAt(i)-'0')==9){
-             answer++;
-         }   
+
+        String str = order+"";
+
+        for(int i=0; i<str.length(); i++){
+            char c = str.charAt(i);
+            if(c=='3'||c=='6'||c=='9') answer++;
         }
+
         return answer;
     }
 }
+
