8534cbad42dc8eaeb11a96be2370793ee7d94bcb
BOSS <65701100+HongMinYeong@users.noreply.github.com>
2024-05-02 18:01:03 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,8 +1,18 @@
+// class Solution {
+//     public int solution(int n) {
+//         int answer = 0;
+//         double num = Math.sqrt(n);
+//         answer=(int)num*(int)num==n?1:2;
+//         return answer;
+//     }
+// }
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        double num = Math.sqrt(n);
-        answer=(int)num*(int)num==n?1:2;
-        return answer;
+        if (n % Math.sqrt(n) == 0) {
+            return 1;
+        } else {
+            return 2;
+        }
     }
 }
+
