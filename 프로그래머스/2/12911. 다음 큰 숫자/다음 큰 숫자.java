3e1675f6f98578c1ff42005e1a7e6d536ee64484
BOSS <65701100+HongMinYeong@users.noreply.github.com>
2024-05-03 22:08:35 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.02 ms, Memory: 51.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String str = Integer.toBinaryString(n);
+        int cnt =0;
+        for(int i = 0;i<str.length();i++){
+            if(str.charAt(i) == '1') cnt++;
+        }
+        for(int i=n+1;i<=1000000;i++){
+            int cnt2=0;
+            String str2 = Integer.toBinaryString(i);
+            for(int j = 0;j<str2.length();j++){
+            if(str2.charAt(j) == '1') cnt2++;
+            }
+            if(cnt==cnt2){
+                answer=i;
+                break;
+            }
+        }
+        return answer;
+    }
+}
