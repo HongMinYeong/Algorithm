0ab6ca0b1458c855a48f4cc95e2fc3f5ff44f312
BOSS <65701100+HongMinYeong@users.noreply.github.com>
2024-05-03 17:53:41 +0900

    [level 2] Title: 올바른 괄호, Time: 8.33 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,53 +1,39 @@
-// import java.util.*;
-
-// class Solution {
-//     boolean solution(String s) {
-//         boolean answer = true;
-//         Stack<Character> stack = new Stack<>();
-//         int cnt = 0;
-        
-//         for(char c: s.toCharArray()){
-//             stack.push(c);
-//         }
-//         char now = stack.pop(); 
-
-//         if (now!=')'){
-//             answer=false;
-//             return answer;
-//         }else{
-//             while(!stack.isEmpty()){
-//                 if(now==')'){
-//                     char next=stack.peek();
-//                     if(next=='('){
-//                         stack.pop();
-//                         cnt++;
-//                         continue;
-//                     }
-//                 }
-//                 now=stack.pop();
-//             }
-//             if(s.length()%2==0){
-//              answer=cnt==s.length()/2?true:false;   
-//             }else{
-//                 answer=false;
-//             }
-//             return answer;
-//         }
-//     }
+// class Solution { 
+// 	boolean solution(String s) { 
+//     	boolean answer = false;
+//         int open = 0, close = 0; //시작과 끝의 괄호 체크
+//         if(s.charAt(0)==')'||s.charAt(s.length()-1)=='(')
+//         	return false;
+//         for(int i=0;i<s.length();i++){
+//         	if(s.charAt(i)=='(') open++;
+//             else close++;
+//             if(open-close==-1) return false;
+//          }
+//          if(open==close) answer= true; 
+//          else answer= false; 
+//          return answer; 
+//      } 
 // }
-class Solution { 
-	boolean solution(String s) { 
-    	boolean answer = false;
-        int open = 0, close = 0; //시작과 끝의 괄호 체크
-        if(s.charAt(0)==')'||s.charAt(s.length()-1)=='(')
-        	return false;
-        for(int i=0;i<s.length();i++){
-        	if(s.charAt(i)=='(') open++;
-            else close++;
-            if(open-close==-1) return false;
-         }
-         if(open==close) answer= true; 
-         else answer= false; 
-         return answer; 
-     } 
+class Solution {
+    boolean solution(String s) {
+        boolean answer = false;
+        int count = 0;
+        for(int i = 0; i<s.length();i++){
+            if(s.charAt(i) == '('){
+                count++;
+            }
+            if(s.charAt(i) == ')'){
+                count--;
+            }
+            if(count < 0){
+                break;
+            }
+        }
+        if(count == 0){
+            answer = true;
+        }
+
+        return answer;
+    }
 }
+
