478f702817948c1459f1d5a7dd9e4003b8913c29
BOSS <65701100+HongMinYeong@users.noreply.github.com>
2024-05-29 14:24:33 +0900

    [Silver I] Title: 현대모비스 소프트웨어 아카데미, Time: 524 ms, Memory: 31048 KB -BaekjoonHub

--- 백준/Silver/26091. 현대모비스 소프트웨어 아카데미/현대모비스 소프트웨어 아카데미.java
+++ 백준/Silver/26091. 현대모비스 소프트웨어 아카데미/현대모비스 소프트웨어 아카데미.java
@@ -10,20 +10,21 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine(), " ");
-        Integer[] arr = new Integer[N];
-        for(int i=0;i<N;i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for(int i=0;i<N;i++){
+            arr.add(Integer.parseInt(st.nextToken()));
         }
-        Arrays.sort(arr);//정렬
+        Collections.sort(arr);//정렬
 
         int sp = 0;
-        int ep = N-1;
+        int ep = arr.size()-1;
         int answer=0;
-
 
         while(sp<ep){
             if(N<2) break;
-            if(arr[sp]+arr[ep]>=M){
+            if(arr.get(sp)+arr.get(ep)>=M){
                 //팀의 능력치보다 같거나 크다면
                 answer++;
                 ep--;
