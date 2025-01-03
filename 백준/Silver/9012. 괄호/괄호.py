5fd9b57178f742d485784b1e163e1e09d4e6d754
hongminyeong <65701100+HongMinYeong@users.noreply.github.com>
2025-01-03 20:25:08 +0900

    [Silver IV] Title: 괄호, Time: 52 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,16 @@
+T = int(input())
+for _ in range(T):
+    stk = []
+    isVPS = True
+    for ch in input():
+        if ch == '(':
+            stk.append(ch)
+        else:
+            if len(stk) >0:
+                stk.pop()
+            else:
+                isVPS = False
+                break
+    if len(stk) > 0 :
+        isVPS = False
+    print('YES' if isVPS else 'NO')
