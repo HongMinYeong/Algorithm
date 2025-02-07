d52890ef2b37ebac67deb33817f06be47ede2826
hongminyeong <65701100+HongMinYeong@users.noreply.github.com>
2025-02-07 14:41:51 +0900

    [Silver II] Title: 이항 계수 2, Time: 368 ms, Memory: 61780 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -0,0 +1,22 @@
+import sys 
+
+MOD = 10007
+
+cache = [[0]* 1001 for _ in range(1001)]
+sys.setrecursionlimit(10**7)
+N,K = map(int,input().split())
+
+def solution(n,k):
+	if cache[n][k]:
+		return cache[n][k]
+		
+	if k== 0 or k == n:
+		cache[n][k] = 1 
+	
+	else:
+		cache[n][k] = solution(n-1,k-1) + solution(n-1,k) 
+	
+	return cache[n][k]
+	
+ 
+print(solution(N,K)%10007)
