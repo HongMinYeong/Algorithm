a04faf6fb55dfe55c5d38c3ffb3b65bfacd37bdf
hongminyeong <65701100+HongMinYeong@users.noreply.github.com>
2025-02-12 21:57:47 +0900

    [Silver I] Title: 포도주 시식, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,33 +1,24 @@
 import sys
 
 N = int(sys.stdin.readline().strip())
-
 arr = [int(sys.stdin.readline().strip()) for _ in range(N)]
 
 if N == 1:
     print(arr[0])
-    sys.exit()
-
+    
 elif N == 2:
     print(arr[0] + arr[1])
-    sys.exit()
-
-
-elif N <= 3:
-    ans = [0] * N
-    ans[0] = arr[0]
-    ans[1] = arr[0] + arr[1]
-    ans[2] = max(arr[0] + arr[2], arr[1] + arr[2])
-    print(max(ans))
-
+    
 else:
     ans = [0] * N
     ans[0] = arr[0]
     ans[1] = arr[0] + arr[1]
     ans[2] = max(arr[0] + arr[2], arr[1] + arr[2])
-
-    ans[3] = max(arr[2]+ans[0]+arr[3],ans[1]+arr[3])
+    
+    if N > 3:
+        ans[3] = max(ans[0] + arr[2] + arr[3], ans[1] + arr[3])
+    
     for i in range(4, N):
-        ans[i] = max(max(ans[i-4] + arr[i-1] + arr[i], ans[i-3] + arr[i-1] + arr[i]),ans[i-2] + arr[i])
+        ans[i] = max(ans[i-4] + arr[i-1] + arr[i], ans[i-3] + arr[i-1] + arr[i], ans[i-2] + arr[i])
     
     print(max(ans))
