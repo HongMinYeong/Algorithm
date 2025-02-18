b7d37abe9c26a37d57dc812121e14c6166420989
hongminyeong <65701100+HongMinYeong@users.noreply.github.com>
2025-02-18 23:26:18 +0900

    [Silver I] Title: 미로 탐색, Time: 68 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,26 +1,34 @@
 from collections import deque
-n,m =map(int,input().split())
-graph = [[] for _ in range(n)]
-
-for i in range(n):
-    k = list(map(int,input()))
-    for j in range(m):
-        graph[i].append(k[j])
 
 
-d = [(0,1),(0,-1),(1,0),(-1,0)]
+dy = (0,1,0,-1)
+dx = (1,0,-1,0)
 
-def bfs(x,y,graph):
-    queue = deque([(x,y)])
-    while queue:
-        x,y = queue.popleft()
-        for dx,dy in d:
-            X, Y = x+dx,y+dy
-            if (0 <= X < n) and (0 <= Y <m) and graph[X][Y]==1:
-                queue.append((X,Y))
-                graph[X][Y]= graph[x][y] + 1
 
-bfs(0,0,graph)
+N, M = map(int, input().split())  
+road = [input() for _ in range(N)]
 
-# 4 6 이면 [3][5]까지 가면됨
-print(graph[n-1][m-1])
+def is_valid_coord(y,x):
+    return 0 <= y < N and 0 <= x < M
+def bfs():
+    chk = [[False] * M for _ in range(N)]
+    chk[0][0] = True
+    dq = deque()
+    dq.append((0,0,1))
+    while dq:
+        y, x, d = dq.popleft()
+
+        if y == N-1 and x == M-1:
+            return d
+        
+        nd = d + 1
+        for k in range(4):
+            ny = y + dy[k]
+            nx = x + dx[k]
+
+            if is_valid_coord(ny,nx) and road[ny][nx]=='1' and not chk[ny][nx]:
+                chk[ny][nx] = True
+                dq.append((ny,nx,nd))
+
+
+print(bfs())
