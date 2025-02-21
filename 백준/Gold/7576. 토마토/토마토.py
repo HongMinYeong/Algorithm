7fd106be5ee1a8c9b3c55ea7c5014ce2d7c1c533
hongminyeong <65701100+HongMinYeong@users.noreply.github.com>
2025-02-21 20:46:33 +0900

    [Gold V] Title: 토마토, Time: 1180 ms, Memory: 99508 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,55 +1,40 @@
 import sys
 from collections import deque
 
-
 input = sys.stdin.readline
+M, N = map(int, input().split())
 
-M, N = map(int,input().split())
-
-tomato = [list(map(int,input().split())) for _ in range(N)]
-
-
-                    
-
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
-
-def bfs():
-    global q
-    chk = [[False for _ in range(M)] for _ in range(N)]
-    answer = 0
-    while q:
-        x,y = q.popleft()
-        # print(x,y)
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < N and 0 <= ny < M and tomato[nx][ny] != -1 and not chk[nx][ny]:
-                chk[nx][ny] = True
-                if tomato[nx][ny] == 0 and tomato[x][y] > 0:
-                    tomato[nx][ny] = tomato[x][y] + 1
-                    answer = max(answer,tomato[x][y])
-                    q.append((nx,ny))
-                
-                    
-    # print(tomato)
-    if sum(row.count(0) for row in tomato)>0:
-        return -1
-    else:
-        return answer
-    
-        
-               
-        
-# 익은 토마토 위치 찾기 
-to = []
+tomato = [list(map(int, input().split())) for _ in range(N)]
 q = deque()
+
+# 익은 토마토 위치 큐에 저장
 for i in range(N):
     for j in range(M):
         if tomato[i][j] == 1:
-            q.append((i,j))
+            q.append((i, j))
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs():
+    answer = 0
+
+    while q:
+        x, y = q.popleft()
+        
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+
+            if 0 <= nx < N and 0 <= ny < M and tomato[nx][ny] == 0:
+                tomato[nx][ny] = tomato[x][y] + 1
+                answer = max(answer, tomato[nx][ny] - 1)  # 최댓값 업데이트
+                q.append((nx, ny))
+
+    # 모든 BFS가 끝난 후 0이 남아있는지 확인
+    for row in tomato:
+        if 0 in row:
+            return -1
+
+    return answer
 
 print(bfs())
-
-            
-
